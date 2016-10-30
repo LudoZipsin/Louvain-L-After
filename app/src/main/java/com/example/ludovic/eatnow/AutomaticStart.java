77d78a4d460070626fe49cda30292c6cb39a0044
Ludovic Zipsin <ludovic.j.r.zipsin@gmail.com>
2016-10-30 15:15:54 +0100

    registering boradcast receiver to launch at start up

--- app/src/main/java/com/example/ludovic/eatnow/AutomaticStart.java
+++ app/src/main/java/com/example/ludovic/eatnow/AutomaticStart.java
@@ -1,6 +1,9 @@
 package com.example.ludovic.eatnow;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
 /**
  * Created by Ludovic Zipsin on 30/10/16.
@@ -8,4 +11,11 @@ import android.content.BroadcastReceiver;
  * Github: https://github.com/LudoZipsin
  */
 public class AutomaticStart extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent){
+        Intent serviceIntent = new Intent(context, BGService.class);
+        context.startService(serviceIntent);
+        Log.i("AutomaticStart", "BGService started");
+    }
+
 }
